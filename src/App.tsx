0849b823932dc5d9d4bb2daaaeb8defb6bb010f6
macbook <12345>
2022-11-29 19:01:37 +0100

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import {LoginPage} from "./pages/auth/presentation/LoginPage";
+import {CartMainPage} from "./pages/cart/presentation/CartMainPage";
+import {MainPage} from "./pages/main_page/presentation/MainPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path={'/'} element={<MainPage/>}/>
+      <Route path={'/login'} element={<LoginPage/>}/>
+      <Route path={'/cart'} element={<CartMainPage/>}/>
+    </Routes>
   );
 }
-
+//   /category/women
 export default App;
---
+++ src/pages/main_page/presentation/MainPage.tsx
@@ -0,0 +1,15 @@
+// @flow
+import * as React from 'react';
+import {Navigation} from "../../../general/navigation/Navigation";
+
+type Props = {
+
+};
+export const MainPage = (props: Props) => {
+    return (
+        <div>
+            <Navigation/>
+            Main page
+        </div>
+    );
+};
