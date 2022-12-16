71cc27e9ceffa1d2f3168b1459df20cb588ab83e
Igor <zorin703@gmail.com>
2022-12-16 23:01:09 +0200

    add signup server

--- src/pages/auth/mirage/authServer.ts
+++ src/pages/auth/mirage/authServer.ts
@@ -1,3 +1,5 @@
+// @ts-ignore
+
 import {createServer} from "miragejs"
 
 createServer({
@@ -7,16 +9,19 @@ createServer({
             let attrs = JSON.parse(request.requestBody)
             const user = attrs.email + attrs.password
             const userReg = 'qqq@qqq.qq1234567'
-
             attrs.loginData = {
                 id: Date.now(),
                 accessToken: '123456789qwerty',
             }
-
-            attrs.errors = {error:'No login'}
-
+            attrs.errors = {error: 'No login'}
             return user == userReg ? attrs.loginData : attrs.errors
+        })
 
+
+        this.post("/api/registration", (schema, request) => {
+            let attrs = JSON.parse(request.requestBody)
+            attrs.errors = {errors: "You have successfully registered!"}
+            return attrs.errors;
         })
     },
 })
